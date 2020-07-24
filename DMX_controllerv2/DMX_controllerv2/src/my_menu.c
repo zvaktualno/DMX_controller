8b5b985193614a5c51453c867b3eb1de51c526a5
tadejzivic <tadej.zivic@hotmail.com>
2020-07-24 07:57:28 +0200

    upgraded formating

--- DMX_controllerv2/DMX_controllerv2/src/my_menu.c
+++ DMX_controllerv2/DMX_controllerv2/src/my_menu.c
@@ -6,8 +6,7 @@
 //#include "functions.h"
 
 
-void menu_add_item(MENU *m, menu_item item)
-{
+void menu_add_item(MENU *m, menu_item item) {
     if(m->num_of_items == MENU_MAX_ITEMS)
         return;
     m->items[m->num_of_items] = item;
@@ -16,79 +15,72 @@ void menu_add_item(MENU *m, menu_item item)
     m->curr_window_pos = 0;
 }
 
-void increment_menu_position(MENU *m)
-{
+void increment_menu_position(MENU *m) {
     if(m->curr_pos < m->num_of_items - 1)
         m->curr_pos++;
     if((m->curr_pos - m->curr_window_pos) == 4)
         m->curr_window_pos++;
 }
 
-void decrement_menu_position(MENU *m)
-{
+void decrement_menu_position(MENU *m) {
     if(m->curr_pos > 0)
         m->curr_pos--;
     if(m->curr_pos < m->curr_window_pos)
         m->curr_window_pos--;
 }
 
-uint8_t get_menu_position(MENU *m)
-{
+uint8_t get_menu_position(MENU *m) {
     return m->curr_pos;
 }
 
-menu_item *get_p_to_item(MENU *m)
-{
+menu_item *get_p_to_item(MENU *m) {
     return &(m->items[m->curr_pos]);
 }
 
-void ftoe(char *s, float val)
-{
+void ftoe(char *s, float val) {
     uint32_t whole = (int) val;
-    float fract = val-whole;
-    uint32_t whole_fract= fract*1000;
+    float fract = val - whole;
+    uint32_t whole_fract = fract * 1000;
     //ne dela v primeru 4.05
-    sprintf(s, "%3lu.%03lu", whole,whole_fract);
+    sprintf(s, "%3lu.%03lu", whole, whole_fract);
 }
 
-void menu_get_item_string(MENU *m, char *str, uint8_t n)
-{
+void menu_get_item_string(MENU *m, char *str, uint8_t n) {
     if(n >= m->num_of_items)
         return;
     menu_item *tmp = m->items + n;
     switch(tmp->type) {
         case UINT8:
-            sprintf(tmp->val_str, "%6d", *(uint8_t *)tmp->variable);
+            sprintf(tmp->val_str, "%7d", *(uint8_t *)tmp->variable);
             break;
         case UINT16:
-            sprintf(tmp->val_str, "%6d", *(uint16_t *)tmp->variable);
+            sprintf(tmp->val_str, "%7d", *(uint16_t *)tmp->variable);
             break;
         case UINT32:
-            sprintf(tmp->val_str, "%6lu", *(uint32_t *)tmp->variable);
+            sprintf(tmp->val_str, "%7lu", *(uint32_t *)tmp->variable);
             break;
         case TYPE_MENU:
-            sprintf(tmp->val_str, "       ");
+            sprintf(tmp->val_str, "%7s", "");
             break;
         case FLOAT:
             ftoe(tmp->val_str, *(float *)tmp->variable);
             break;
         default:
-            strcpy(tmp->val_str, "ERROR99");
+            strcpy(tmp->val_str, "%7s", "ERROR99");
             break;
     }
     *(tmp->val_str + 7) = 0;
     char test[21];
     if(n == m->curr_pos) {
-        sprintf(test, "%c%s%s%s", '~', tmp->name, tmp->val_str + 1, tmp->units);
+        sprintf(test, "%c%-9s%6s%4s", '~', tmp->name, tmp->val_str, tmp->units);
     }
     else
-        sprintf(test, "%s%s%s", tmp->name, tmp->val_str, tmp->units);
+        sprintf(test, "%-9s%7s%4s", tmp->name, tmp->val_str, tmp->units);
     memcpy(str, test, 20);
 
 }
 
-void menu_whole_solo_string(MENU *m, char *s)
-{
+void menu_whole_solo_string(MENU *m, char *s) {
     char tmp_string[21];
     menu_item *p_to_item = get_p_to_item(m);
     menu_get_item_string(m, tmp_string, m->curr_pos);
@@ -111,8 +103,7 @@ void menu_whole_solo_string(MENU *m, char *s)
     memcpy(s + 42 + value_length + 1, p_to_item->units, strlen(p_to_item->units));
 }
 
-void menu_whole_string(MENU *m, char *s, STATE state)
-{
+void menu_whole_string(MENU *m, char *s, STATE state) {
     if(state == EDIT) {
         menu_whole_solo_string(m, s);
     }
@@ -128,21 +119,19 @@ void menu_whole_string(MENU *m, char *s, STATE state)
 
 }
 
-void menu_create_item(menu_item *item, const char *name, enum VAR_TYPE typ, const char *units, void *p_variable, float min_val, float max_val)
-{
+void menu_create_item(menu_item *item, const char *name, enum VAR_TYPE typ, const char *units, void *p_variable, float min_val, float max_val) {
     strcpy(item->name, "         ");
     strcpy(item->name, name);
     strcpy(item->units, "    ");
     strcpy(item->units, units);
-    strcpy(item->val_str,"       ");
+    strcpy(item->val_str, "       ");
     item->type = typ;
     item->variable = p_variable;
     item->val_max = max_val;
     item->val_min = min_val;
 }
 
-void menu_increment_item(MENU *m)
-{
+void menu_increment_item(MENU *m) {
     switch(m->items[m->curr_pos].type) {
         case UINT8:
             if((*(uint8_t *)m->items[m->curr_pos].variable) < (m->items[m->curr_pos].val_max))
@@ -164,8 +153,7 @@ void menu_increment_item(MENU *m)
             break;
     }
 }
-void menu_decrement_item(MENU *m)
-{
+void menu_decrement_item(MENU *m) {
     switch(m->items[m->curr_pos].type) {
         case UINT8:
             if((*(uint8_t *)m->items[m->curr_pos].variable) > (m->items[m->curr_pos].val_min))
@@ -187,7 +175,6 @@ void menu_decrement_item(MENU *m)
             break;
     }
 }
-void menu_swap(MENU **dest, MENU *src)
-{
+void menu_swap(MENU **dest, MENU *src) {
     *dest = src;
 }
